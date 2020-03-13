610f978e75c8314350af295540e2423280be7e36
unknown <danielc@NSPP78212.corporate.int>
2020-03-13 13:45:36 -0300

    alteração do layout

--- src/app/eventos-edit/eventos-edit.component.ts
+++ src/app/eventos-edit/eventos-edit.component.ts
@@ -29,11 +29,7 @@ export class EventosEditComponent implements OnInit {
                                         .dpefetivacao .myDpIconLeftArrow,
                                         .myDpIconRightArrow {
                                                 color: red;
-                                            },
-                                            .myDpSelectorAbsolute {
-                                              position: relative !important;
-                                            }  
-
+                                            }
                                         `
                                     } 
                                     };
